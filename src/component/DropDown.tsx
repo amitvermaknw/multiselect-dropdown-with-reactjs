1db496cb6cd0e2afd925bbb98ce9ca4370ca2f34
Amit Verma <amitfromus@gmail.com>
2024-01-22 22:29:09 -0700

    modified component for multiselect

--- src/component/DropDown.tsx
+++ src/component/DropDown.tsx
@@ -3,7 +3,17 @@ import { DropDownItem } from './DropdownItem';
 import { DropDownItemProps } from '../utilities/DropDownInterface';
 
 const styles = stylex.create({
-
+    base: {
+        fontSize: 16,
+        lineHeight: 1.5
+    },
+    select: {
+        width: '100%',
+        height: '100%',
+        border: 'solid 1px rgb(0, 0, 0, 0.7)',
+        borderRadius: '2px',
+        padding: '5px',
+    }
 })
 
 type DropDownProps = {
@@ -12,12 +22,15 @@ type DropDownProps = {
 
 export function DropDownComponent({ props }: DropDownProps) {
     return (
-        <>
-            {props.length ? <div><select>
+        <div {...styles.base}>
+            <select {...styles.select}>
+                <option>Select</option>
+            </select>
+            {props.length ? <div>
                 {
-                    props.map((item: DropDownItemProps) => <DropDownItem {...item} />)
+                    props.map((item: DropDownItemProps) => <DropDownItem {...item} key={item.id} />)
                 }
-            </select></div> : ''}
-        </>
+            </div> : ''}
+        </div>
     )
 }
--- src/component/DropdownItem.tsx
+++ src/component/DropdownItem.tsx
@@ -3,9 +3,11 @@ import { DropDownItemProps } from "../utilities/DropDownInterface";
 
 export function DropDownItem({ id, value, label }: DropDownItemProps) {
     return (
-        <div>
-            <input type="checkbox" id={id as string} />
-            <option value={value} key={id}>{label}</option>
-        </div>
+        <>
+            <label key={label + "_" + id}>
+                <input type="checkbox" id={id as string} key={value + "_" + id} value={value} />
+                {value}
+            </label>
+        </>
     )
 }
